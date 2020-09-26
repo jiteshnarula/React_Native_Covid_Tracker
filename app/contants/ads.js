5964251969048bd0f81cc952c832e0a28d25aabe
Jitesh Narula <jiteshnarula21@gmail.com>
2020-09-27 03:13:36 +0530

    updated apname to ctracker

--- app/contants/ads.js
+++ app/contants/ads.js
@@ -1,3 +1,3 @@
 export const App_ID = 'ca-app-pub-7763902047214535~3229481229';
 export const Android_Add_Unit =
-  'ca-app-pub-7763902047214535/5280929492';
+  'ca-app-pub-7763902047214535/1777610824';

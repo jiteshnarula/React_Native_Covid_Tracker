ceff4713c07b15523897762a093af53d1133d598
jiteshnarula <jiteshnarula21@gmail.com>
2021-01-05 01:36:08 +0530

    updating appid

--- app/contants/ads.js
+++ app/contants/ads.js
@@ -1,3 +1,3 @@
-export const App_ID = 'ca-app-pub-7763902047214535~3229481229';
+export const App_ID = 'ca-app-pub-7763902047214535~2747640583';
 export const Android_Add_Unit =
-  'ca-app-pub-7763902047214535/1777610824';
+  'ca-app-pub-7763902047214535/1241141311';
